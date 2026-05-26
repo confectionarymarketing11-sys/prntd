9001421ce9bfba978b75694aec3e7698d47b8828
Mario Vitali <febrezeasy@outlook.com.>
2026-05-26 10:58:39 -0400

    sdfsd

--- app/design-generator/page.tsx
+++ app/design-generator/page.tsx
@@ -689,9 +689,9 @@ const editInterval =
       await navigator.mediaDevices.getUserMedia(
         {
           audio: {
-  echoCancellation: true,
-  noiseSuppression: true,
-  autoGainControl: true,
+  echoCancellation: false,
+  noiseSuppression: false,
+  autoGainControl: false,
   channelCount: 1,
   sampleRate: 48000,
 }
