aac3fb7a256d9a809373c8fb4f3960b02cdb6fe1
Mario Vitali <febrezeasy@outlook.com.>
2026-05-26 11:18:58 -0400

    lol

--- app/design-generator/page.tsx
+++ app/design-generator/page.tsx
@@ -859,7 +859,7 @@ let silenceStart =
 
 const silenceThreshold = 6;
 
-const silenceDelay = 1200;
+const silenceDelay = 1800;
 
 function checkSilence() {
   if (
@@ -899,7 +899,7 @@ function checkSilence() {
       Date.now();
   }
 
-  ///*if (
+  /*if (
     Date.now() -
       silenceStart >
     silenceDelay
@@ -909,7 +909,7 @@ function checkSilence() {
     audioContext.close();
 
     return;
-  }*\\\
+  }*\
 
   requestAnimationFrame(
     checkSilence,
@@ -921,6 +921,7 @@ checkSilence();
     console.error(
       "Microphone failed:",
       error,
+console.log(volume);
     );
 
     setVoiceListening(false);
